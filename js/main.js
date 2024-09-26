98e00a979b876e635325d304e2117be1eda7a015
ashraf <99658659+ashrafShaaban@users.noreply.github.com>
2024-09-26 13:50:22 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,3 +15,17 @@ menu.querySelectorAll(".dropdown > i").forEach((arrow)=>{
      this.closest(".dropdown").classList.toggle("active");
     });
 });
+
+function sendMail(){
+    let parms ={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("number").value,
+        message: document.getElementById("message").value,
+    }
+    emailjs.send("service_i6xmfpe","template_az41clx",parms).then(alert("We have Received your mail"));
+    document.getElementById("name").value="";
+    document.getElementById("email").value="";
+    document.getElementById("number").value="";
+    document.getElementById("message").value="";
+ };
